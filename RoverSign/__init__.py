f8d997c8ac52845116432a6ea770d6867cbcafe0
Loping151 <wangkailing151@gmail.com>
2025-12-06 15:53:15 +0800

    就ww

--- RoverSign/__init__.py
+++ RoverSign/__init__.py
@@ -2,4 +2,4 @@
 
 from gsuid_core.sv import Plugins
 
-Plugins(name="RoverSign", force_prefix=["rs"], allow_empty_prefix=False)
+Plugins(name="RoverSign", force_prefix=["ww"], allow_empty_prefix=False)
